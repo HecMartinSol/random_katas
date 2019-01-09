70e6138f09d3615c74b432e1d771aacc91d86e4b
Héctor Martín <HecMartinSol@users.noreply.github.com>
2019-01-09 09:36:59 +0100

    Add files via upload

---
+++ python/product_fibonacci.py
@@ -0,0 +1,65 @@
+"""
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prodyou will return
+
+[F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(m) being the smallest one such as F(m) * F(m+1) > prod.
+
+Examples
+productFib(714) # should return [21, 34, true], 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return [34, 55, false], 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+Notes: Not useful here but we can tell how to choose the number n up to which to go: we can use the "golden ratio" phi which is (1 + sqrt(5))/2 knowing that F(n) is asymptotic to: phi^n / sqrt(5). That gives a possible upper bound to n.
+
+You can see examples in "Example test".
+
+References
+http://en.wikipedia.org/wiki/Fibonacci_number
+
+http://oeis.org/A000045
+
+
+https://www.codewars.com/kata/product-of-consecutive-fib-numbers/train/python
+"""
+
+
+
+
+def productFib(prod):
+    found = False
+    fib_prod = 0
+    fib_curr = 0
+    fib_next = 1
+    while not found and fib_prod < prod:
+    	fib_prod = fib_curr * fib_next
+
+    	found = fib_prod == prod
+    	if not found and fib_prod < prod:
+	    	new_fib = fib_curr + fib_next;
+	    	fib_curr = fib_next
+	    	fib_next = new_fib
+    		pass
+    	pass
+    return [fib_curr, fib_next, found]
+
+
+print(productFib(4895)) # == [55, 89, True])
+print(productFib(5895)) # == [89, 144, False])
