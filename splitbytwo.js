c019abcf6644144d6fc23d44ba651decc1539952
Héctor Martín <HecMartinSol@users.noreply.github.com>
2020-01-29 11:49:21 +0100

    Delete splitbytwo.js

--- splitbytwo.js
+++
@@ -1,6 +0,0 @@
-// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/solutions/javascript
-function solution(str) {
-    str_a = str.split("")
-    if (str_a.length % 2 != 0) str_a.push("_")
-    return str_a.join("").match(/.{2}/g);
-}
