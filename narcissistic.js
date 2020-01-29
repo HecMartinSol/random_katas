ea9ea056abe7eca213d6b449e63f080fe6816f53
Héctor Martín <HecMartinSol@users.noreply.github.com>
2020-01-29 11:49:30 +0100

    Delete narcissistic.js

--- narcissistic.js
+++
@@ -1,5 +0,0 @@
-//https://www.codewars.com/kata/5287e858c6b5a9678200083c/solutions/javascript
-function narcissistic(value) {
-    var digits = String(value).split("");
-    return digits.map(x => Math.pow(parseInt(x), digits.length)).reduce((acc, x) => acc + x) == value;
-}
